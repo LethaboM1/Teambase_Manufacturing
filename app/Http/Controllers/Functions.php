6e42e012e2906b1b3f1b0f6ff1758768e2e1db10
Duncan <duncanm@digitalextreme.co.za>
2023-07-12 08:34:43 +0200

    x

--- app/Http/Controllers/Functions.php
+++ app/Http/Controllers/Functions.php
@@ -13,7 +13,8 @@ class Functions extends Controller
         switch ($type) {
             case 'batch':
                 $settings = ManufactureSettings::first();
-                ManufactureSettings::get()->update(['batch_number' => DB::raw("@doc_number := batch_number+1")]);
+
+                DB::table('manufacture_settings')->update(['batch_number' => DB::raw("@doc_number := batch_number+1")]);
                 $number = DB::select(DB::raw("select @doc_number as number"));
                 $number = $number[0]->number;
 
