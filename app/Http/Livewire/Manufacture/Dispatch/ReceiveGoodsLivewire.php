45201c265a3c2d57f85d367d7a56b015b323f2c6
Duncan <duncanm@digitalextreme.co.za>
2023-09-06 09:28:08 +0200

    x

--- app/Http/Livewire/Manufacture/Dispatch/ReceiveGoodsLivewire.php
+++ app/Http/Livewire/Manufacture/Dispatch/ReceiveGoodsLivewire.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Manufacture\Dispatch;
 
 use App\Http\Controllers\SelectLists;
+use App\Models\ManufactureProducts;
 use App\Models\ManufactureSuppliers;
 use Livewire\Component;
 
@@ -10,10 +11,13 @@ class ReceiveGoodsLivewire extends Component
 {
     public function render()
     {
-        $supplier_list = ManufactureSuppliers::select('id as value', 'name')->get();
+        $supplier_list = ManufactureSuppliers::select('id as value', 'name')->get()->toArray();
         array_unshift($supplier_list, SelectLists::empty_select);
+
+        $products_list = ManufactureProducts::select("id as value", "concat(code,' - ',description') as name")->where('has_recipe', 0)->get();
         return view('livewire.manufacture.dispatch.receive-goods-livewire', [
-            'supplier_list' => $supplier_list
+            'supplier_list' => $supplier_list,
+            'products_list' => $products_list,
         ]);
     }
 }
--- resources/views/livewire/manufacture/dispatch/receive-goods-livewire.blade.php
+++ resources/views/livewire/manufacture/dispatch/receive-goods-livewire.blade.php
@@ -3,7 +3,7 @@
         <x-form.select name="supplier_id" label="Supplier" :list="$supplier_list" />
     </div>
     <div class="col-md-6">
-        
+        <x-form.select name="product_id" label="Products" :list="$products_list" />
     </div>
     
 </div>
