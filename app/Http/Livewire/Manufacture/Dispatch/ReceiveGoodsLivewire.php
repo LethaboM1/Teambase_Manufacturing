175225a1044fec1306c302a460dd1307068f1584
Duncan <duncanm@digitalextreme.co.za>
2023-09-06 09:24:48 +0200

    x

--- app/Http/Livewire/Manufacture/Dispatch/ReceiveGoodsLivewire.php
+++ app/Http/Livewire/Manufacture/Dispatch/ReceiveGoodsLivewire.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Manufacture\Dispatch;
 
+use App\Http\Controllers\SelectLists;
 use App\Models\ManufactureSuppliers;
 use Livewire\Component;
 
@@ -10,6 +11,7 @@ class ReceiveGoodsLivewire extends Component
     public function render()
     {
         $supplier_list = ManufactureSuppliers::select('id as value', 'name')->get();
+        array_unshift($supplier_list, SelectLists::empty_select);
         return view('livewire.manufacture.dispatch.receive-goods-livewire', [
             'supplier_list' => $supplier_list
         ]);
