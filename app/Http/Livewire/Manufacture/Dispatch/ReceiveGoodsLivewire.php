3eb447b8ad16abcf9ad8885c0ebf40ff69117435
Duncan <duncanm@digitalextreme.co.za>
2023-09-06 09:28:29 +0200

    x

--- app/Http/Livewire/Manufacture/Dispatch/ReceiveGoodsLivewire.php
+++ app/Http/Livewire/Manufacture/Dispatch/ReceiveGoodsLivewire.php
@@ -14,7 +14,7 @@ class ReceiveGoodsLivewire extends Component
         $supplier_list = ManufactureSuppliers::select('id as value', 'name')->get()->toArray();
         array_unshift($supplier_list, SelectLists::empty_select);
 
-        $products_list = ManufactureProducts::select("id as value", "concat(code,' - ',description') as name")->where('has_recipe', 0)->get();
+        $products_list = ManufactureProducts::select("id as value", "concat(code,' - ',description) as name")->where('has_recipe', 0)->get();
         return view('livewire.manufacture.dispatch.receive-goods-livewire', [
             'supplier_list' => $supplier_list,
             'products_list' => $products_list,
