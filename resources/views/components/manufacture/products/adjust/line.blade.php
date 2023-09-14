e9eb34180a92a5fe5fbc945335a7fbf1993d517b
Duncan <duncanm@digitalextreme.co.za>
2023-09-14 08:56:00 +0200

    x

--- resources/views/components/manufacture/products/adjust/line.blade.php
+++ resources/views/components/manufacture/products/adjust/line.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td>{{$line->created_at}}</td>
     <td>{{$line->qty}}</td>
-    <td>{{$user->name}} {{$user->last_name}}</td>
+    <td>{{(isset($user->name)?$user->name:'')}} {{(isset($user->last_name)?$user->last_name:'')}}</td>
     <td>{{$line->comment}}</td>
     <td></td>
 </tr>
--- resources/views/livewire/manufacture/dispatch/receive-goods-livewire.blade.php
+++ resources/views/livewire/manufacture/dispatch/receive-goods-livewire.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-md-6">
-        <x-form.input wire=0 name="reference_number" label="Deliver Number" />
+        <x-form.input wire=0 name="reference_number" label="Reference Number" />
     </div>
     <div class="col-md-6">
         <x-form.input wire=0 name="registration_number" label="Registration Number" />        
@@ -10,9 +10,6 @@
     </div>
     <div class="col-md-6">
         <x-form.select wire=0 name="product_id" label="Products" :list="$products_list" />
-    </div>
-    <div class="col-md-6">
-        <x-form.datetime wire=0 name="weight_in_datetime" label="Date Time" :value="date('Y-m-d\TH:i')" />
     </div>
     <div class="col-md-6">
         <x-form.number wire=0 name="weight_in" label="Weight" step="0.001" />
