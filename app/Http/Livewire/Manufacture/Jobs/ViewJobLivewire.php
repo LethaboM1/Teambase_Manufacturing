b382a7037017bb9e446955ab7dda7e9dd3f92d79
Duncan <duncanm@digitalextreme.co.za>
2023-10-03 11:20:18 +0200

    x

--- app/Http/Livewire/Manufacture/Jobs/ViewJobLivewire.php
+++ app/Http/Livewire/Manufacture/Jobs/ViewJobLivewire.php
@@ -87,7 +87,7 @@ class ViewJobLivewire extends Component
     public function render()
     {
         $customer_list = [];
-        $customer_list = ManufactureCustomers::select('id as value', DB::raw("concat(account_number,' - ',name) as name"))
+        $customer_list = ManufactureCustomers::select('id as value', DB::raw("name"))
             ->get()
             ->toArray();
 
