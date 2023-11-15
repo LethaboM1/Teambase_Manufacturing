726d33b952bbf27735b82a0741b88542b729e90e
Duncan <duncanm@digitalextreme.co.za>
2023-11-15 16:43:19 +0200

    x

--- app/Http/Controllers/Manufacture/BatchesController.php
+++ app/Http/Controllers/Manufacture/BatchesController.php
@@ -47,17 +47,6 @@ class BatchesController extends Controller
 
         if ($recipe->count() == 0)  return back()->with('alertError', 'No recipe for this product. Create your recipe first.');
 
-        // foreach ($recipe as $item) {
-        //     $qty = ($item->qty * $form_fields['qty']);            
-        //     ManufactureProductTransactions::insert([
-        //         'product_id' => $item->product_add_id,
-        //         'type' => 'BAT',
-        //         'qty' => $qty,
-        //         'user_id' => auth()->user()->user_id,
-        //         'status' => ''
-        //     ]);
-        // }
-
         $batch_id = ManufactureBatches::insertGetId($form_fields);
 
         foreach ($recipe as $item) {
